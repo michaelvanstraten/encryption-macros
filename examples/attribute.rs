7a102275424be80a0a625d6ae784719325a40363
Michael van Straten <michael@vanstraten.de>
2022-04-08 20:31:50 +0200

    fixed bug failing to expand macro when literal is not string literal

--- examples/attribute.rs
+++ examples/attribute.rs
@@ -2,6 +2,5 @@ use encryption_macros::encrypt_all_strings;
 
 #[encrypt_all_strings]
 fn main() {
-    let num = 2244;
     println!("everything in this function gets encrypted, {}", "even this")
 }
